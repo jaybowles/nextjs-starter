81196feb54f6d4d038fa58279379f87208b9ec85
Albert Chen <albert.chen@me.com>
2018-09-20 19:44:05 -0400

    Implemented API

--- app/routes/api/inventory.js
+++ app/routes/api/inventory.js
@@ -1,6 +1,8 @@
 import { Router } from 'express';
+import Eos from 'eosjs';
 
 const router = Router();
+const eos = Eos();
 
 const STATIC_DATA = [
   {
@@ -10,6 +12,28 @@ const STATIC_DATA = [
   }
 ];
 
-router.get('/', (req, res) => res.json({ data: STATIC_DATA }));
+router.get('/:user', (req, res) => {
+  const { user } = req.params;
+
+  eos
+    .getTableRows({
+      json: true,
+      code: 'eosio.nft', // Contract who owns the table
+      scope: 'eosio.nft', // Scope of the table
+      table: 'token'
+    })
+    .then(({ rows }) => {
+      const result = rows.filter(row => row.owner === user).map(row => {
+        const temp = JSON.parse(row.uri);
+        return {
+          id: row.id,
+          owner: row.owner,
+          name: row.name,
+          temp
+        };
+      });
+      res.json(result);
+    });
+});
 
 export default router;
